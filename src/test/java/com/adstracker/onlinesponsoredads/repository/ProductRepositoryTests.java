8170226d7692fb2d7d7bf45d2bde13cc454ca060
aymanab5 <aymansce@gmail.com>
2023-07-01 22:46:22 +0300

    test get all products repo

--- src/test/java/com/adstracker/onlinesponsoredads/repository/ProductRepositoryTests.java
+++ src/test/java/com/adstracker/onlinesponsoredads/repository/ProductRepositoryTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -16,16 +17,33 @@ public class ProductRepositoryTests {
     @Test
     public void ProductRepository_Save_ReturnSavedProduct() {
         //Arrange
-        Product product = Product.builder()
-                .productName("MacBook Pro")
-                .productCategory("Gaming")
-                .productPrice(1999.9)
-                .build();
-
+        Product product = saveNewProduct("MacBook Pro","Gaming",1999.99);
         //Act
         Product savedproduct = productRepo.save(product);
         //Assert
         Assertions.assertThat(savedproduct).isNotNull();
         Assertions.assertThat(savedproduct.getProductId()).isGreaterThan(0);
+    }
+    @Test
+    public void ProductRepository_GetAll_ReturnMoreThenOneProduct() {
+        //Arrange
+        Product product1 = saveNewProduct("MacBook Pro","Gaming",1999.99);
+        Product product2 = saveNewProduct("Canon EOS 5D Mark IV", "Electronics", 2499.99);
+        //Act
+        productRepo.save(product1);
+        productRepo.save(product2);
+        List<Product> productList = productRepo.findAll();
+        //Assert
+        Assertions.assertThat(productList).isNotNull();
+        //When the server was initiated, a total of 26 products were successfully preserved(from data.sql).
+        Assertions.assertThat(productList.size()).isEqualTo(28);
+    }
+    public Product saveNewProduct(String productName , String ProductCategory , Double productPrice){
+        Product product = Product.builder()
+                .productName(productName)
+                .productCategory(ProductCategory)
+                .productPrice(productPrice)
+                .build();
+        return product;
     }
 }
